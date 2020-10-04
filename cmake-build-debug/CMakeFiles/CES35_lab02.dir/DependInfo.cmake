c22c5e7366f3566c7a9fe422a35fb766f36b5e0b
Pedro Alves <alvesouza.pedro97@gmail.com>
2020-10-04 09:21:41 -0300

    adcionado templates

---
+++ cmake-build-debug/CMakeFiles/CES35_lab02.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pedro/Documents/github/CES35-lab02/echo-client.cpp" "/home/pedro/Documents/github/CES35-lab02/cmake-build-debug/CMakeFiles/CES35_lab02.dir/echo-client.cpp.o"
+  "/home/pedro/Documents/github/CES35-lab02/echo-server.cpp" "/home/pedro/Documents/github/CES35-lab02/cmake-build-debug/CMakeFiles/CES35_lab02.dir/echo-server.cpp.o"
+  "/home/pedro/Documents/github/CES35-lab02/main.cpp" "/home/pedro/Documents/github/CES35-lab02/cmake-build-debug/CMakeFiles/CES35_lab02.dir/main.cpp.o"
+  "/home/pedro/Documents/github/CES35-lab02/multi-thread.cpp" "/home/pedro/Documents/github/CES35-lab02/cmake-build-debug/CMakeFiles/CES35_lab02.dir/multi-thread.cpp.o"
+  "/home/pedro/Documents/github/CES35-lab02/showip.cpp" "/home/pedro/Documents/github/CES35-lab02/cmake-build-debug/CMakeFiles/CES35_lab02.dir/showip.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CES35_lab02.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CES35_lab02"
+  "CES35_lab02.pdb"
+  "CMakeFiles/CES35_lab02.dir/echo-client.cpp.o"
+  "CMakeFiles/CES35_lab02.dir/echo-server.cpp.o"
+  "CMakeFiles/CES35_lab02.dir/main.cpp.o"
+  "CMakeFiles/CES35_lab02.dir/multi-thread.cpp.o"
+  "CMakeFiles/CES35_lab02.dir/showip.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CES35_lab02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
