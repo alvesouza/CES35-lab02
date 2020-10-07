8b7eaf94c1283bd8d2fe24c4ef7b0cd3b4c3942b
Pedro Alves <alvesouza.pedro97@gmail.com>
2020-10-07 11:24:09 -0300

    configurações do CMake, falta apagar os templates

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "functions/ip/ip_functions.h"
+#include "functions/Inc/ip_functions.h"
 
 int main(int argc, char *argv[]) {
     std::cout << "teste ip adress = "<< get_ip_from_hostname(argv[1]) << std::endl;
