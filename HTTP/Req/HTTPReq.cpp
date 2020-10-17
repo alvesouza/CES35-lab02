f2560e03b83274a7ed4e167d8d6468b09dc4350b
joao <joaocfariaf@gmail.com>
2020-10-17 16:31:38 -0300

    setUrl

--- HTTP/Req/HTTPReq.cpp
+++ HTTP/Req/HTTPReq.cpp
@@ -4,17 +4,45 @@
 
 #include "HTTPReq.h"
 #include "string.h"
+#include "stdlib.h"
+
 
 HTTPReq::HTTPReq() {
-    this->url = new std::string;
-
 }
 
-void HTTPReq::setURL(std::string url_to_be) {
+std::pair<std::basic_string<char>, std::basic_string<char>> HTTPReq::setURL(const char* url_to_be) {
     this->url = url_to_be;
+
+    // "Decoding" url
+    // http://
+    // 0123456
+    if (!this->url.compare(0, 6,"http://")) {
+        return std::make_pair("-1", "-1");
+    }
+    int len = this->url.length();
+    int j = 7;
+    int i = j;
+    while (this->url.at(i) != ':' && i < len){
+        i++;
+    }
+    this->host =  this->url.substr(j, i-1);
+    j = i;
+    while (this->url.at(i) != '/' && i < len){
+        i++;
+    }
+    this->port = this->url.substr(j, i-1);
+    this->obj = this->url.substr(i, len);
+
+    return std::make_pair(host, port);
 }
 
 const char* HTTPReq::enconde() {
-    return this->url.c_str();
+    this->url.c_str();
 
+    return
+}
+
+int HTTPReq::makeRequest() {
+
+    return 0;
 }
--- HTTP/Req/HTTPReq.h
+++ HTTP/Req/HTTPReq.h
@@ -6,16 +6,23 @@
 #define CES35_LAB02_HTTPREQ_H
 #include <string>
 #include <vector>
+#include "../../functions/Inc/ip_functions.h"
+
 class HTTPReq {
+    std::basic_string<char> host;
+    std::basic_string<char> port;
+    std::basic_string<char> obj;
     std::string url;
     HTTPReq();
 
 public:
     // Coloca o url passada como argumento no obj da classe
-    void setURL(std::string url_to_be);
+    // Coloca o obj na classe e retorna as strings do host e da porta, como par
+    std::pair<std::basic_string<char>, std::basic_string<char>> setURL(const char* enteredUrl);
     // Não precisa estar implementado aqui, pois o método é sempre GET
     // void setMethod(std::string url);
     const char* enconde();
+    int makeRequest();
 };
 
 
