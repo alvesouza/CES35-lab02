5fc257d767fa8f1fe231f6809db6753b8a4275a4
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-10-18 20:26:18 -0300

    Add web-client

--- Client/web-client.cpp
+++ Client/web-client.cpp
@@ -6,108 +6,123 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <thread>
 
 #include <iostream>
 #include <sstream>
+
+#include "web-client.h"
+#include "ip_functions.h"
+#include "constants.h"
 
 typedef u_short content_type;
 
 #define APP_JSON 0
 #define TEXT_HTML 1
 
-typedef struct HTTPReq HTTPReq;
-struct HTTPReq{
-    std::string url;
+webClient::webClient(const char* url){
+    req = new HTTPReq(url);
+}
 
-};
+webClient::~webClient(){
+    // fecha o socket
+    close(sockfd);
+}
 
-int main() {
-  // cria o socket TCP IP
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+int webClient::run(){
+    // cria o socket TCP IP
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-  // atividade de preenchimento da estrutura de endereço IP
-  // e porta de conexão, precisa ser zerado o restante da estrutura
-  // struct sockaddr_in {
-  //  short            sin_family;   // e.g. AF_INET, AF_INET6
-  //  unsigned short   sin_port;     // e.g. htons(3490)
-  //  struct in_addr   sin_addr;     // see struct in_addr, below
-  //  char             sin_zero[8];  // zero this if you want to
-  // };
-  struct sockaddr_in serverAddr;
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(40000);     // short, network byte order
-//    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.2.3");
-  memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
+    // atividade de preenchimento da estrutura de endereço IP
+    // e porta de conexão, precisa ser zerado o restante da estrutura
+    // struct sockaddr_in {
+    //  short            sin_family;   // e.g. AF_INET, AF_INET6
+    //  unsigned short   sin_port;     // e.g. htons(3490)
+    //  struct in_addr   sin_addr;     // see struct in_addr, below
+    //  char             sin_zero[8];  // zero this if you want to
+    // };
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(stoi(req->getPort())); // short, network byte order
+    serverAddr.sin_addr.s_addr = inet_addr(get_ip_from_hostname(req->getHost().c_str()).c_str());
+    memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
+    
+    return connection();
+}
 
-  // conecta com o servidor atraves do socket
-  if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
-    perror("init_listener");
-    return 2;
-  }
-
-  // a partir do SO, eh possivel obter o endereço IP usando
-  // pelo cliente (nos mesmos) usando a funcao getsockname
-  struct sockaddr_in clientAddr;
-  socklen_t clientAddrLen = sizeof(clientAddr);
-  if (getsockname(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen) == -1) {
-    perror("getsockname");
-    return 3;
-  }
-
-  // em caso de multiplos endereços, isso permite o melhor controle
-  // a rotina abaixo, imprime o valor do endereço IP do cliente
-  char ipstr[INET_ADDRSTRLEN] = {'\0'};
-  inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
-  std::cout << "Set up a connection from: " << ipstr << ":" <<
-    ntohs(clientAddr.sin_port) << std::endl;
-
-  // trecho de código para leitura e envio de dados nessa conexao
-  // buffer eh o buffer de dados a ser recebido no socket com 20 bytes
-  // input eh para a leitura do teclado
-  // ss eh para receber o valor de volta
-  bool isEnd = false;
-  char buf[20] = {0};
-  std::string input;
-  std::stringstream ss;
-
-  while (!isEnd) {
-    // zera o buffer
-    memset(buf, '\0', sizeof(buf));
-
-    // leitura do teclado
-    std::cout << "send: ";
-    std::cin >> input;
-
-    // converte a string lida em vetor de bytes
-    // com o tamanho do vetor de caracteres
-    if (send(sockfd, input.c_str(), input.size(), 0) == -1) {
-      perror("send");
-      return 4;
+int webClient::connection(){
+    // conecta com o servidor atraves do socket
+    if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+        perror("connect");
+        return 2;
     }
 
-    // recebe no buffer uma certa quantidade de bytes ate 20
-    if (recv(sockfd, buf, 20, 0) == -1) {
-      perror("recv");
-      return 5;
+    // a partir do SO, eh possivel obter o endereço IP usando 
+    // pelo cliente (nos mesmos) usando a funcao getsockname
+    struct sockaddr_in clientAddr;
+    socklen_t clientAddrLen = sizeof(clientAddr);
+    if (getsockname(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen) == -1) {
+        perror("getsockname");
+        return 3;
     }
 
-    // coloca o conteudo do buffer na string
-    // imprime o buffer na tela
-    ss << buf << std::endl;
-    std::cout << "echo: ";
-    std::cout << buf << std::endl;
+    // em caso de multiplos endereços, isso permite o melhor controle
+    // a rotina abaixo, imprime o valor do endereço IP do cliente
+    char ipstr[INET_ADDRSTRLEN] = {'\0'};
+    inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
+    std::cout << "Set up a connection from: " << ipstr << ":" <<
+        ntohs(clientAddr.sin_port) << std::endl;
 
-    // se a string tiver o valor close, sair do loop de echo
-    if (ss.str() == "close\n")
-      break;
+    return waitResponse();
+}
 
-    // zera a string ss
-    ss.str("");
-  }
+int webClient::waitResponse(){
+    // buffer eh o buffer de dados a ser recebido no socket com 1MB
+    bool isEnd = false;
+    char buf[buffer_max_size] = {0};
+    std::string input;
+    std::stringstream ss;
+    while (!isEnd) {
+        
+        // zera o buffer
+        memset(buf, '\0', sizeof(buf));
 
-  // fecha o socket
-  close(sockfd);
+        // leitura do teclado
+        std::cout << "send: ";
+        std::cin >> input;
 
-  return 0;
+        // envia a request
+        if (send(sockfd, req->serialize().c_str(), req->getBytecode().size(), 0) == -1) {
+            perror("send");
+            return 4;
+        }
+        
+        // recebe no buffer uma certa quantidade de bytes ate 1MB
+        if (recv(sockfd, buf, buffer_max_size, 0) == -1) {
+            perror("recv");
+            return 5;
+        }
+        resp = new HTTPResp(buf);
+        resp->deserializeResp();
+        resp->saveFile();
+        
+        // coloca o conteudo do buffer na string
+        // imprime o buffer na tela
+        ss << buf << std::endl;
+        std::cout << "echo: ";
+        std::cout << buf << std::endl;
+
+        // se a string tiver o valor close, sair do loop de echo
+        if (ss.str() == "close\n")
+        break;
+
+        // zera a string ss
+        ss.str("");
+    }
+
+    return 0;
+}
+
+int main(int argc, const char** argv) {
+    webClient* client = new webClient(argv[1]);
+    return client->run();;
 }
--- Client/web-client.h
+++ Client/web-client.h
@@ -3,18 +3,26 @@
 #include <string>
 #include <vector>
 
-#include "HTTPResp.h"
-#include "HTTPReq.h"
+#include "../HTTP/Req/HTTPReq.h"
+#include "../HTTP/Resp/HTTPResp.h"
 
-class client {
+class webClient {
+private:
+    HTTPReq* req;
+    HTTPResp* resp;
     std::string url;
-
+    int sockfd;
+    struct sockaddr_in serverAddr;
 public:
-    // Coloca o url passada como argumento no obj da classe
-    void setURL(std::string url_to_be);
-    // Não precisa estar implementado aqui, pois o método é sempre GET
-    // void setMethod(std::string url);
-    const char* enconde();
+    webClient(const char* url);
+    
+    int run();
+
+    int connection();
+
+    int waitResponse();
+
+    ~webClient();
 };
 
 
