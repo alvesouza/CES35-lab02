02ab2fbd7b3d442dc49f45a1dac3c33d112ab135
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-10-18 20:26:43 -0300

    Fix on constants.h

--- functionals/Inc/constants.h
+++ functionals/Inc/constants.h
@@ -6,7 +6,7 @@
 #define CES35_LAB02_CONSTANTS_H
 
 #include "iostream"
-const size_t buffer_max_size = 10000060;
-const size_t payload_max_size = 10000000;
+const size_t buffer_max_size = 1048580;
+const size_t payload_max_size = 1048580;
 
 #endif //CES35_LAB02_CONSTANTS_H
