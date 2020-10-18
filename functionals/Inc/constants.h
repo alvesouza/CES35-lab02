b24a54458568689f670a475f6292b913f4cb164d
Pedro Alves <alvesouza.pedro97@gmail.com>
2020-10-18 18:58:32 -0300

    constants.h feito

--- functionals/Inc/constants.h
+++ functionals/Inc/constants.h
@@ -5,4 +5,8 @@
 #ifndef CES35_LAB02_CONSTANTS_H
 #define CES35_LAB02_CONSTANTS_H
 
+#include "iostream"
+const size_t buffer_max_size = 10000060;
+const size_t payload_max_size = 10000000;
+
 #endif //CES35_LAB02_CONSTANTS_H
