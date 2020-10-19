e8924902d8d8286666ee748bc6c7e4afac7bc3a1
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-10-18 23:00:51 -0300

    Delete main.cpp

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "functions/Inc/ip_functions.h"
-
-int main(int argc, char *argv[]) {
-    std::cout << "teste ip adress = "<< get_ip_from_hostname(argv[1]) << std::endl;
-    return 0;
-}
